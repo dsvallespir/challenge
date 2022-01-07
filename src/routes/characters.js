c0f87d3f802bb0349a283caeffbdf5a72e86ee9a
dsvallespir <dsvallespir@gmail.com>
2022-01-07 18:22:38 -0300

    Primer commit: falta trabajarlo

--- src/routes/characters.js
+++ src/routes/characters.js
@@ -1,14 +1,12 @@
 const { Router } = require('express');
 const router = Router();
 const imageUpload = require('../lib/multer');
-const Resize = require('../lib/resize');
+//const Resize = require('../lib/resize');
 const path = require('path');
 const fs = require('fs');
 
 
 const pool = require('../database');
-const { restart } = require('nodemon');
-const { pathToFileURL } = require('url');
 
 router.get('/characters', (req, res) => {
     res.send('characters');
@@ -41,33 +39,6 @@ router.post('/characters/add', imageUpload.single('image-file'), (req, res) => {
     });
 
 });
-/*    
-    const { nombre, edad, peso, imageUpload} = req.body;
-    const imagePath = path.join(__dirname, '/public/images');
-    const fileUpload = new Resize(imagePath);  
-    if(!imageUpload){
-        res.status(401).json({error: 'Please provide an image'});
-    }
-    if(imageUpload.buffer){
-
-        const filename = await fileUpload.save(imageUpload.buffer);
-        //console.log(req.body);
-        //res.send('received');
-        const newCharacter = {
-            filename,
-            nombre,
-            edad, 
-            peso
-        }
-        await pool.query('INSERT INTO personajes set ?', [newCharacter]);
-        return res.status(200).json({name: filename});
-    }
-    else{
-        console.log("Error: el buffer está vacío");
-    }
-    */
-
-
 
 module.exports = router;
 
